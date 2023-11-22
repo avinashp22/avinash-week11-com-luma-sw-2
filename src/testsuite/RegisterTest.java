124e26ae2686813ad43410e1632c6c990e175c49
avinashp22 <avinashp22@gmail.com>
2023-11-22 23:36:00 +0000

    avinash-week11-com-luma-sw-2

--- src/testsuite/RegisterTest.java
+++ src/testsuite/RegisterTest.java
@@ -2,8 +2,10 @@ package testsuite;
 
 import browserfactory.BaseTest;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class RegisterTest extends BaseTest {
 
@@ -14,11 +16,35 @@ public class RegisterTest extends BaseTest {
 
     @Test
     public void verifyThatSignInPageDisplay(){
+        driver.findElement(By.linkText("Create an Account")).click();
+        String expectedText = "Create New Customer Account";
+        String actualText = driver.findElement(By.xpath("//span[text()='Create New Customer Account']")).getText();
+        Assert.assertEquals(expectedText, actualText);
+
     }
 
 
     @Test
     public void userShouldRegisterAccountSuccessfully(){
+        driver.findElement(By.linkText("Create an Account")).click();
+
+        driver.findElement(By.id("firstname")).sendKeys("Michael");
+        driver.findElement(By.id("lastname")).sendKeys("Jordan");
+        driver.findElement(By.id("email_address")).sendKeys("michaelljjordan@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("Prime123!");
+        driver.findElement(By.id("password-confirmation")).sendKeys("Prime123!");
+        driver.findElement(By.xpath("//span[text()='Create an Account']")).click();
+
+        String expectedText = "Thank you for registering with Main Website Store.";
+        String actualText = driver.findElement(By.xpath("//div[text()='Thank you for registering with Main Website Store.']")).getText();
+        Assert.assertEquals(expectedText, actualText);
+
+        driver.findElement(By.xpath("//button[@class='action switch']")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]")).click();
+        String expectedMessage = "You are signed out";
+        String actualMessage = driver.findElement(By.xpath("//span[text()='You are signed out']")).getText();
+        Assert.assertEquals(expectedMessage, actualMessage);
+
 
     }
 
--- src/testsuite/SaleTest.java
+++ src/testsuite/SaleTest.java
@@ -2,8 +2,13 @@ package testsuite;
 
 import browserfactory.BaseTest;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class SaleTest extends BaseTest {
 
@@ -15,11 +20,24 @@ public class SaleTest extends BaseTest {
     @Test
     public void verifyTheTotalItemsDisplayedOnTheWomensJacketsPage(){
 
+        driver.findElement(By.xpath("//span[text() = 'Sale']")).click();
+        driver.findElement(By.xpath("//a[@href='https://magento.softwaretestingboard.com/women/tops-women/jackets-women.html'][text()='Jackets']")).click();
 
+        String expected = "Jackets";
+        String actual = driver.findElement(By.xpath("//span[@class = 'base']")).getText();
+        Assert.assertEquals("Correct Message not Displayed",expected,actual);
+
+        List<WebElement> items = driver.findElements(By.xpath("//li[@class = 'item product product-item']"));
+        int expectedCount = 12;
+        int actualCount = items.size();
+        System.out.println("Expected Items : 12");
+        System.out.println("Actual Items : "+items.size());
+        Assert.assertEquals("Correct Amount not Displayed",expectedCount,actualCount);
+
+        List<WebElement> itemsName = driver.findElements(By.xpath("//a[@class = 'product-item-link']"));
+        for(WebElement list : itemsName){System.out.println(list.getText());}
 
     }
-
-
 
 
     @After
